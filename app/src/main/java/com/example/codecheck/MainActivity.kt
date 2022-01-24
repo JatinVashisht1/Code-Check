50ac9593f5d0ef97cb0f07b701a9c5a1c3bf2aa5
JatinVashisht1 <jatin8085vashisht@gmail.com>
2022-01-24 20:30:37 +0530

    Added Dependencies

--- app/src/main/java/com/example/codecheck/MainActivity.kt
+++ app/src/main/java/com/example/codecheck/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.codecheck
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
