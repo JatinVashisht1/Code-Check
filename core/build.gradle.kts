30588b2e79dc8747bc376aefc4b1d4e3b645cbd5
JatinVashisht1 <jatin8085vashisht@gmail.com>
2022-01-29 16:57:55 +0530

    Migrated from groovy to kotlin dsl

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         targetSdk = 31
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-//        consumerProguardFiles = "consumer-rules.pro"
+        consumerProguardFiles ("consumer-rules.pro")
     }
 
     buildTypes {
